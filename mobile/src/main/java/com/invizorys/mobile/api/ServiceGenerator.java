1a0827a5a4726f68957d0430b6c511be0fb60231
Alexey Zyabkin <alexis.zyabkin@gmail.com>
2015-12-13 01:22:13 +0200

    GSON date format fix

--- mobile/src/main/java/com/invizorys/mobile/api/ServiceGenerator.java
+++ mobile/src/main/java/com/invizorys/mobile/api/ServiceGenerator.java
@@ -4,6 +4,8 @@ import com.squareup.okhttp.OkHttpClient;
 
 import retrofit.RestAdapter;
 import retrofit.client.OkClient;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 /**
  * Created by Paryshkura Roman on 12.12.2015.
@@ -13,9 +15,15 @@ public class ServiceGenerator {
     }
 
     public static <T> T createService(Class<T> serviceClass, String baseUrl) {
+        Gson gson = new GsonBuilder()
+                .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+                .create();
+
         RestAdapter.Builder builder = new RestAdapter.Builder()
                 .setEndpoint(baseUrl)
+                .setConverter(new GsonConverter(gson))
                 .setClient(new OkClient(new OkHttpClient()));
+
         RestAdapter restAdapter = builder.build();
 
         return restAdapter.create(serviceClass);
