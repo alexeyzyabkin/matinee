2ad20e740eebf72ac5faddca4ad0b0084713c670
Bogdan Pogoriliy <bpogoriliy@gmail.com>
2015-12-12 18:50:10 +0200

    add jpa annotations in TaskProgress (part 2) add pohorilyi.properties

--- backend/src/main/java/com/letionik/matinee/model/TaskProgress.java
+++ backend/src/main/java/com/letionik/matinee/model/TaskProgress.java
@@ -2,6 +2,7 @@ package com.letionik.matinee.model;
 
 import com.letionik.matinee.TaskStatus;
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 /**
@@ -14,8 +15,50 @@ public class TaskProgress {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "task_progress_id")
     private Long id;
+    @OneToOne(mappedBy = "task_progress")
     private Task task;
+    @Column(name = "task_progress_executive_time")
     private LocalDateTime executiveTime;
+    @OneToOne(mappedBy = "task_progress")
     private Participant participant;
+    @NotNull
+    @Enumerated
+    @Column(name = "task_progress_task_status")
     private TaskStatus status = TaskStatus.NEW;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Task getTask() {
+        return task;
+    }
+
+    public void setTask(Task task) {
+        this.task = task;
+    }
+
+    public LocalDateTime getExecutiveTime() {
+        return executiveTime;
+    }
+
+    public void setExecutiveTime(LocalDateTime executiveTime) {
+        this.executiveTime = executiveTime;
+    }
+
+    public Participant getParticipant() {
+        return participant;
+    }
+
+    public void setParticipant(Participant participant) {
+        this.participant = participant;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
 }
