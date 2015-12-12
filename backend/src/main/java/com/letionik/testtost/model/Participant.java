749f945e6a32d557df07735a524d21726748f04d
Guzya <irynaguzenko1@gmail.com>
2015-12-12 17:56:57 +0200

    add JPA annotations (part 1)

--- backend/src/main/java/com/letionik/testtost/model/Participant.java
+++ backend/src/main/java/com/letionik/testtost/model/Participant.java
@@ -1,15 +1,66 @@
 package com.letionik.testtost.model;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Iryna Guzenko on 12.12.2015.
  */
+@Entity
+@Table(name = "participant")
 public class Participant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "participant_id")
     private Long id;
+    @NotNull
+    @OneToOne(mappedBy = "participant")
     private User user;
+    @NotNull
+    @OneToOne(mappedBy = "participant")
     private Role role;
+    @OneToMany(mappedBy = "participant")
     private List<Task> tasks = new ArrayList<>();
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "participant_event_id")
     private Event event;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
