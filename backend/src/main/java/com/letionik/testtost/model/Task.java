eac4e25680515974b4aecc75c109749cf7786dc3
Bogdan Pogoriliy <bpogoriliy@gmail.com>
2015-12-12 17:58:33 +0200

    add jpa annotations (part 2)

--- backend/src/main/java/com/letionik/testtost/model/Task.java
+++ backend/src/main/java/com/letionik/testtost/model/Task.java
@@ -2,12 +2,63 @@ package com.letionik.testtost.model;
 
 import com.letionik.testtost.TaskType;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
 /**
  * Created by Iryna Guzenko on 12.12.2015.
  */
+@Entity
+@Table(name = "task")
 public class Task {
+    @Id
+    @Column(name = "task_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    @Column(name = "task_name")
     private String name;
+
+    @NotNull
+    @Column(name = "task_description")
     private String description;
-    private TaskType type;
+
+    @NotNull
+    @Column(name = "task_type")
+    @Enumerated(EnumType.STRING)
+    private TaskType taskType;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "task_participant_id")
+    private Participant participant;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public TaskType getTaskType() {
+        return taskType;
+    }
+
+    public void setTaskType(TaskType taskType) {
+        this.taskType = taskType;
+    }
 }
--- backend/src/main/java/com/letionik/testtost/model/TaskPerfomance.java
+++ backend/src/main/java/com/letionik/testtost/model/TaskPerfomance.java
@@ -2,13 +2,20 @@ package com.letionik.testtost.model;
 
 import com.letionik.testtost.TaskStatus;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 /**
  * Created by Iryna Guzenko on 12.12.2015.
  */
+@Entity
+@Table(name = "task_perfomance")
 public class TaskPerfomance {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "task_perfomance_id")
     private Long id;
+
     private Task task;
     private LocalDateTime executiveTime;
     private Participant participant;
