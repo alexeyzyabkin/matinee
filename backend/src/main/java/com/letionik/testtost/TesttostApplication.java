508b7e5237d3ea2f1ae8e9d44087e5cb4090305a
Alexey Zyabkin <alexis.zyabkin@gmail.com>
2015-12-12 09:57:40 +0200

    init commit

---
+++ backend/src/main/java/com/letionik/testtost/TesttostApplication.java
@@ -0,0 +1,12 @@
+package com.letionik.testtost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesttostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesttostApplication.class, args);
+    }
+}
---
+++ model/src/test/java/com/letionik/testtost/TestTest.java
@@ -0,0 +1,7 @@
+package com.letionik.testtost;
+
+/**
+ * Created by Alexey Zyabkin on 12.12.2015.
+ */
+public class TestTest {
+}
