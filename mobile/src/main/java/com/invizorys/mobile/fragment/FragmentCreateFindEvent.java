0e3929135fd11c845d6c9e72feada19519b3a63e
Alexey Zyabkin <alexis.zyabkin@gmail.com>
2015-12-13 02:22:32 +0200

    Enroll event code

--- mobile/src/main/java/com/invizorys/mobile/fragment/FragmentCreateFindEvent.java
+++ mobile/src/main/java/com/invizorys/mobile/fragment/FragmentCreateFindEvent.java
@@ -6,13 +6,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.invizorys.mobile.R;
 import com.invizorys.mobile.activity.MainActivity;
+import com.invizorys.mobile.api.MatineeService;
+import com.invizorys.mobile.api.RetrofitCallback;
+import com.invizorys.mobile.api.ServiceGenerator;
 import com.invizorys.mobile.util.FragmentHelper;
+import com.letionik.matinee.EventDto;
+
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 public class FragmentCreateFindEvent extends Fragment implements View.OnClickListener {
     private FragmentManager fragmentManager;
+    private EditText enrollCodeText;
+    private MatineeService matineeService;
 
     public static FragmentCreateFindEvent newInstance() {
         return new FragmentCreateFindEvent();
@@ -36,6 +46,8 @@ public class FragmentCreateFindEvent extends Fragment implements View.OnClickLis
 
         fragmentManager = getActivity().getFragmentManager();
 
+        matineeService = ServiceGenerator.createService(MatineeService.class, MatineeService.BASE_URL);
+        enrollCodeText = (EditText) view.findViewById(R.id.enroll_code_text);
         return view;
     }
 
@@ -47,6 +59,21 @@ public class FragmentCreateFindEvent extends Fragment implements View.OnClickLis
                 FragmentHelper.add(fragmentManager, FragmentCreateEvent.newInstance(),
                         MainActivity.FRAME_CONTAINER);
                 break;
+            case R.id.button_enroll_event:
+                String code = enrollCodeText.getText().toString();
+                matineeService.enroll(code, new RetrofitCallback<EventDto>(getActivity()) {
+                    @Override
+                    public void success(EventDto eventDto, Response response) {
+                        String ff = "";
+                        FragmentHelper.add(fragmentManager, FragmentEvent.newInstance(),
+                                MainActivity.FRAME_CONTAINER);
+                    }
+
+                    @Override
+                    public void failure(RetrofitError error) {
+                        super.failure(error);
+                    }
+                });
         }
     }
 }
