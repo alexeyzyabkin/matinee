385553a0f8d1d8d988e2c8476c23c72e6d95234e
Alexey Zyabkin <alexis.zyabkin@gmail.com>
2015-12-13 02:11:38 +0200

    REST client implementation

--- mobile/src/main/java/com/invizorys/mobile/api/MatineeService.java
+++ mobile/src/main/java/com/invizorys/mobile/api/MatineeService.java
@@ -2,10 +2,15 @@ package com.invizorys.mobile.api;
 
 import com.letionik.matinee.CreateEventRequestDto;
 import com.letionik.matinee.EventDto;
+import com.letionik.matinee.TaskProgressDto;
 import com.letionik.matinee.UserDto;
 
+import java.util.List;
+
 import retrofit.http.Body;
+import retrofit.http.GET;
 import retrofit.http.POST;
+import retrofit.http.Path;
 
 /**
  * Created by Paryshkura Roman on 12.12.2015.
@@ -17,6 +22,21 @@ public interface MatineeService {
     void register(@Body UserDto userDto, RetrofitCallback<UserDto> userDtoRetrofitCallback);
 
     @POST("/event")
-    void  createEvent(@Body CreateEventRequestDto createEventRequestDto,
-                      RetrofitCallback<EventDto> eventDtoRetrofitCallback);
+    void createEvent(@Body CreateEventRequestDto createEventRequestDto,
+                     RetrofitCallback<EventDto> eventDtoRetrofitCallback);
+
+    @POST("/event/enroll/{code}")
+    void enroll(@Path("code") String code, RetrofitCallback<EventDto> userDtoRetrofitCallback);
+
+    @POST("/event/reveal/tasks/{eventId}")
+    void revealTasks(@Path("eventId") String eventId, RetrofitCallback<EventDto> userDtoRetrofitCallback);
+
+    @POST("/event/reveal/roles/{eventId}")
+    void revealRoles(@Path("eventId") String eventId, RetrofitCallback<EventDto> userDtoRetrofitCallback);
+
+    @POST("/event/history/{eventId}")
+    void getHistory(@Path("eventId") String eventId, RetrofitCallback<List<TaskProgressDto>> userDtoRetrofitCallback);
+
+    @GET("/event/{eventId}")
+    void getCurrentEvent(@Path("eventId") String eventId, RetrofitCallback<EventDto> userDtoRetrofitCallback);
 }
