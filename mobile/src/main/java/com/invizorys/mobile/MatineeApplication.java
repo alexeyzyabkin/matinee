1571673c1c46064f0d978248923b9c71faf472b2
Parishkura Roman <parishkura.roma@gmail.com>
2016-01-02 13:50:24 +0200

    implemented correct showing first screen

---
+++ mobile/src/main/java/com/invizorys/mobile/MatineeApplication.java
@@ -0,0 +1,39 @@
+package com.invizorys.mobile;
+
+import android.app.Application;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
+import com.invizorys.mobile.ui.activity.LoginActivity;
+import com.invizorys.mobile.ui.activity.MainActivity;
+import com.invizorys.mobile.util.Settings;
+import com.vk.sdk.VKSdk;
+
+/**
+ * Created by Paryshkura Roman on 12.12.2015.
+ */
+public class MatineeApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        VKSdk.initialize(this);
+
+        Context context = getApplicationContext();
+        Intent loginIntent = new Intent(context, LoginActivity.class);
+        Intent mainIntent = new Intent(context, MainActivity.class);
+        String token = Settings.fetchToken(context);
+        PendingIntent pendingIntent;
+        if (token == null || token.isEmpty()) {
+            pendingIntent = PendingIntent.getActivity(context, 22, loginIntent, 0);
+        } else {
+            pendingIntent = PendingIntent.getActivity(context, 22, mainIntent, 0);
+        }
+        try {
+            pendingIntent.send();
+        } catch (PendingIntent.CanceledException e) {
+            e.printStackTrace();
+        }
+    }
+}
